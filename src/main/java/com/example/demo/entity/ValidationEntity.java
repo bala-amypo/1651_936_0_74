5b65debeda52eed20c8e99228f84bc5de6f37f47
bala-amypo <bala@amypo.in>
2025-12-18 09:48:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -1,12 +1,13 @@
 package com.example.demo.entity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ValidationEntity;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Email;
 @Entity
 public class ValidationEntity{
     @Id
@@ -48,7 +49,7 @@ public String getPassword(){
 public void setPassword(String Password){
     this.password=password;
 }
-public Ingeter getAge(){
+public Integer getAge(){
     return age;
 }
 public void setAge(Integer age){
@@ -57,7 +58,7 @@ public void setAge(Integer age){
 public ValidationEntity(Long Id,@NotNull @Size(min=2, max=10, message="must be 2 to 10 character") String username,
                         @Email(message="Email is not valid") String email,
                         @Size(min=0,max=10) @NotNull(message="Password is mandatory") String password,
-                        @Max(30)@Positive(message="Age must be positive") Integer age
+                        @Max(30)@Positive(message="Age must be positive") int age
                         ){
                             this.id=id;
                             this.name=name;
