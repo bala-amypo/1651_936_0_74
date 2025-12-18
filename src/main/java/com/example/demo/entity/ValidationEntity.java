4396167900802002b7137c34585bc73fe777f90a
bala-amypo <bala@amypo.in>
2025-12-18 08:23:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -13,13 +13,23 @@ public class ValidationEntity{
     private Long id;
     @NotNull
     @Size(min=2, max=10, message="must be 2 to 10 character")
-    private String name;
+    private String username;
     @Email(message="Email is not valid")
     private String email;
     @Size(min=0,max=10)
     @NotNull(message="Password is mandatory")
     private String password;
     @Max(30)
+    @Positive(message="Age must be positive")
     private Integer age;
 }
-public ValidationEntity(Long Id,@NotNull @Size(min=2, max=10, message="must be 2 to 10 character") S)
+public ValidationEntity(Long Id,@NotNull @Size(min=2, max=10, message="must be 2 to 10 character") String username,
+                        @Email(message="Email is not valid") String email,
+                        @Size(min=0,max=10) @NotNull(message="Password is mandatory") String password,
+                        @Max(30)@Positive(message="Age must be positive") Integer age
+                        ){
+                            this.id=id;
+                            this.name=name;
+                            this.email=email;
+                            this.password=password;
+                        }
