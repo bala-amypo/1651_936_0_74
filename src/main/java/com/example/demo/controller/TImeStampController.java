678d4f6768a26b6d372381e706003e2427c463a4
bala-amypo <bala@amypo.in>
2025-12-19 08:23:18 +0000

    User commit

--- src/main/java/com/example/demo/controller/TImeStampController.java
+++ src/main/java/com/example/demo/controller/TImeStampController.java
@@ -1,5 +1,30 @@
 package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.example.demo.entity.TimeStampControllerEntity;
+import com.example.demo.service.TimeStampService;
+import java.util.List;
 
+@RestController
 public class TimeStampController{
-    
-}
+
+    @Autowired TimeStampService ser;
+
+
+
+    @PostMapping("/post")
+    public StudentEntity sendData(@RequestBody TimeStampEntity stu){
+        return ser.postData(stu);
+
+
+    }
+}    
