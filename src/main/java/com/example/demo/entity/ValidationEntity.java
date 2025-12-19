43a1aca2c0ba7823bbb211ed83749e302c46cba4
bala-amypo <bala@amypo.in>
2025-12-19 08:41:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -12,6 +12,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 @Entity
 @Data
 @Getter
--- src/main/java/com/example/demo/repository/TimeStampRepo.java
+++ src/main/java/com/example/demo/repository/TimeStampRepo.java
@@ -2,10 +2,10 @@ package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.ValidationEntity;
+import com.example.demo.entity.TimeStampEntity;
 
 @Repository
-public interface TimeStampRepo extends JpaRepostiory<TimeStampEntity,Long>{
+public interface TimeStampRepo extends JpaRepository<TimeStampEntity,Long>{
 
 }
 
