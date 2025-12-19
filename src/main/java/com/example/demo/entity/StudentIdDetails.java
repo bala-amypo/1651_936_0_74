db7a00c2c65801bad5cf0ff862c0db7384c1307f
bala-amypo <bala@amypo.in>
2025-12-19 10:14:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentIdDetails.java
+++ src/main/java/com/example/demo/entity/StudentIdDetails.java
@@ -20,7 +20,6 @@ public class StudentIdDetails{
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String idcardnumber;
-    private String student_id;
     @OneToOne
     @JoinColumn(name="student_id")
     private StudentDetails student;
